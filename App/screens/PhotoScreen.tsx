e6d6e3709d98437496272c5a2da04628f4db6fee
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-06-13 18:46:47 +0500

    Add send post, refactor

--- App/screens/PhotoScreen.tsx
+++ App/screens/PhotoScreen.tsx
@@ -1,11 +1,11 @@
-import {Image, ScrollView, StyleSheet, TouchableOpacity, useWindowDimensions} from 'react-native';
-import {View} from '../components/Themed';
+import {Image, ScrollView, StyleSheet, TextInput, TouchableOpacity, useWindowDimensions} from 'react-native';
+import {Text, View} from '../components/Themed';
 import {PhotoType, RootTabScreenProps} from '../types';
 import {useState} from "react";
 import * as ImagePicker from "expo-image-picker";
 import {MaterialIcons} from "@expo/vector-icons";
 import * as Haptics from 'expo-haptics';
-import * as url from "url";
+import Token from "../token";
 
 export default function PhotoScreen({navigation}: RootTabScreenProps<'Photo'>) {
     const {width} = useWindowDimensions();
@@ -41,19 +41,62 @@ export default function PhotoScreen({navigation}: RootTabScreenProps<'Photo'>) {
         navigation.navigate('Zoom', {openedImage: image});
     }
 
+    const [text, onChangeText] = useState("");
+
+    const sendPost = async () => {
+        if (!image) {
+            fetch(`https://api.vk.com/method/wall.post?owner_id=-199514280&from_group=1&message=${text}&access_token=${Token}&v=5.131`)
+                .then((response) => response.json())
+                .then((data) => console.log(data.response.post_id))
+        }
+        const uploadUrl = await fetch(`https://api.vk.com/method/photos.getWallUploadServer?group_id=199514280&access_token=${Token}&v=5.131`)
+            .then(response => response.json())
+            .then(data => data.response.upload_url)
+        const formData = new FormData();
+        // @ts-ignore
+        formData.append("photo", { uri: image.url, type: 'image/jpeg', name: "photo.jpg"});
+        const photoData = await fetch(uploadUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'multipart/form-data'},
+            body: formData,
+        })
+            .then(response => response.json())
+        const uploadPhotoData = await fetch(`https://api.vk.com/method/photos.saveWallPhoto?group_id=199514280&photo=${photoData.photo}&server=${photoData.server}&hash=${photoData.hash}&access_token=${Token}&v=5.131`)
+            .then(response => response.json())
+            .then(data => data.response[0])
+        fetch(`https://api.vk.com/method/wall.post?owner_id=-199514280&from_group=1&message=${text}&attachments=photo${uploadPhotoData.owner_id}_${uploadPhotoData.id}&access_token=${Token}&v=5.131`)
+            .then((response) => response.json())
+            .then((data) => {
+                setImage(null);
+                onChangeText("");
+                console.log(data.response.post_id);
+            })
+    }
+
+
     return (
         <View lightColor="#eee" darkColor="#1b1b1b" style={styles.pageContainer}>
             <ScrollView contentContainerStyle={styles.container}>
                 {image && <TouchableOpacity activeOpacity={1} onPress={() => onOpenImage(image)}>
-                    <Image source={{uri: image.url}} style={{width: width, height: width * image.height / image.width}}/>
+                    <Image source={{uri: image.url}}
+                           style={{width: width, height: width * image.height / image.width}}/>
                 </TouchableOpacity>
                 }
+                <View style={styles.buttonWrapper}>
+                    <TextInput style={styles.input} onChangeText={onChangeText} value={text}/>
+                </View>
                 <View style={styles.buttonWrapper}>
                     <TouchableOpacity style={styles.button} onPress={pickImage}>
                         <MaterialIcons name="add-photo-alternate" size={24} color="white"/>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button} onPress={openCamera}>
                         <MaterialIcons name="add-a-photo" size={24} color="white"/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonWrapper}>
+                    <TouchableOpacity style={styles.button} onPress={sendPost}>
+                        <Text style={{color: "white", marginRight: 10}}>Опубликовать</Text>
+                        <MaterialIcons name="add-photo-alternate" size={24} color="white"/>
                     </TouchableOpacity>
                 </View>
             </ScrollView>
@@ -83,10 +126,20 @@ const styles = StyleSheet.create({
         width: "100%"
     },
     button: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
         marginVertical: 10,
         padding: 12,
         backgroundColor: "#00b6ea",
         borderRadius: 5,
+    },
+    input: {
+        width: "90%",
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     },
     image: {
         flex: 1,
--- App/screens/PhotosScreen.tsx
+++ App/screens/PhotosScreen.tsx
@@ -1,12 +1,11 @@
 import {Image, ScrollView, StyleSheet, TouchableOpacity, useWindowDimensions} from 'react-native';
 import {Text, View} from '../components/Themed';
 import {PhotoType, RootTabScreenProps} from "../types";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Token from "../token";
 import * as Haptics from "expo-haptics";
 
 export default function PhotosScreen({navigation}: RootTabScreenProps<'Profile'>) {
-    const {width} = useWindowDimensions();
     const [posts, setPosts] = useState<any>([])
 
     useEffect(() => {
@@ -38,6 +37,20 @@ export default function PhotosScreen({navigation}: RootTabScreenProps<'Profile'>
             });
     }, [])
 
+    return (
+        <View lightColor="#eee" darkColor="#1b1b1b" style={styles.pageContainer}>
+            <ScrollView contentContainerStyle={styles.container}>
+                {
+                    posts.map((post: any) => <Post post={post} navigation={navigation}/>)
+                }
+            </ScrollView>
+        </View>
+    );
+}
+
+const Post = ({post, navigation}: any) => {
+    const {width} = useWindowDimensions();
+
     const onOpenImage = (image: any) => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
         // @ts-ignore
@@ -45,22 +58,16 @@ export default function PhotosScreen({navigation}: RootTabScreenProps<'Profile'>
     }
 
     return (
-        <View lightColor="#eee" darkColor="#1b1b1b" style={styles.pageContainer}>
-            <ScrollView contentContainerStyle={styles.container}>
-                {
-                    posts.map((post: any) =>
-                        <View key={post.id}>
-                            <Text>{post.text}</Text>
-                            {post.photos.map((photo: any) => <TouchableOpacity key={photo.id} activeOpacity={1}
-                                                                               onPress={() => onOpenImage(photo)}>
-                                <Image source={{uri: photo.url}}
-                                       style={{height: width * photo.height / photo.width, width: width}}
-                                />
-                            </TouchableOpacity>)}
-                        </View>
-                    )
-                }
-            </ScrollView>
+        <View style={styles.postContainer} key={post.id}>
+            {
+                !!post.text && <Text style={styles.textContainer}>{post.text}</Text>
+            }
+            {post.photos.map((photo: any) => <TouchableOpacity key={photo.id} activeOpacity={1}
+                                                               onPress={() => onOpenImage(photo)}>
+                <Image source={{uri: photo.url}}
+                       style={{height: width * photo.height / photo.width, width: width}}
+                />
+            </TouchableOpacity>)}
         </View>
     );
 }
@@ -76,6 +83,13 @@ const styles = StyleSheet.create({
     container: {
         display: "flex",
         flexDirection: "column",
-        alignItems: "center"
+        alignItems: "center",
+        marginVertical: -5
+    },
+    postContainer: {
+        marginVertical: 5
+    },
+    textContainer: {
+        padding: 10,
     }
 });
